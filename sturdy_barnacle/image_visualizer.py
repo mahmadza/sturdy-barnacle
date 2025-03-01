912e4a4fa085fbd60012c21c39ec6ef8abcf0417
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2025-03-01 12:49:52 +0800

    chore: added print statement for OCR text

--- sturdy_barnacle/image_visualizer.py
+++ sturdy_barnacle/image_visualizer.py
@@ -1,15 +1,17 @@
 import json
+
 import matplotlib.pyplot as plt
 from PIL import Image
+
 from sturdy_barnacle.db_utils import DatabaseManager
 
 
 class ImageVisualizer:
     """Class to visualize images and their metadata from the database."""
-    
+
     def __init__(self, db: DatabaseManager):
         self.db = db
-        
+
     def display_image(self, image_path: str) -> None:
         try:
             img = Image.open(image_path)
@@ -30,10 +32,13 @@ class ImageVisualizer:
 
         self.display_image(img_metadata.image_path)
 
-        detected_objects = json.loads(img_metadata.detected_objects) if img_metadata.detected_objects else {}
+        detected_objects = (
+            json.loads(img_metadata.detected_objects)
+            if img_metadata.detected_objects
+            else {}
+        )
         print(f"Detected Objects: {detected_objects}")
         print(f"Caption: {img_metadata.description or 'No caption available'}")
-
-        # Print metadata
+        print(f"OCR Text: {img_metadata.ocr_text or 'No text detected'}")
         print(f"Date Taken: {img_metadata.datetime or 'Unknown'}")
         print("-" * 50)
